d9106e377a1ceda8bb1152a5e3c3adab56bce92d
B. Drawe <brycerdrawe.b@gmail.com>
2020-09-05 23:56:27 -0600

    all the current weather is displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ var weatherUpdate = document.querySelector("#weather-update")
 var tempId = document.getElementById("tempId");
 var humidId = document.getElementById("humidId");
 var windId = document.getElementById("windId");
-var uvId = document.getElementById("uvId");
+var uvId = document.getElementById("uvID");
+var descriptionId = document.getElementById("description");
 
 
 var searchFormHandler = function(){
@@ -20,13 +21,14 @@ var searchFormHandler = function(){
 }
 var getCurrentWeather =  function(cityName){
   // format the github api url
-  var apiUrl = "http://api.openweathermap.org/data/2.5/forcast?q=" + cityName + "&units=imperial&appid=7d0096a167ea1572df8a347cb4cb0104";
+  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=7d0096a167ea1572df8a347cb4cb0104";
 
   //make a request to the url
   fetch(apiUrl).then(function(response){
       if(response.ok){
       response.json().then(function(data){
          displayWeather(data, cityName);
+         console.log(data);
       });
     } else {
         alert("Error: " + response.statusText);
@@ -37,31 +39,51 @@ var getCurrentWeather =  function(cityName){
     });
 };
 var displayWeather =  function(data, cityName){
+
+    var unixDate = data.dt;
+    var date = new Date(unixDate * 1000);
+    var currDate = date.toLocaleDateString();
+
+    // var hours = date.getHours();
+    // var minutes
     
     // weatherContainer.textContent="";
-    weatherUpdate.textContent = cityName;
+    weatherUpdate.textContent = cityName + " | " + currDate;
 
     //grab all of the data for each item
+
     //TEMP
     var weatherTemp = data.main.temp;
-    console.log(weatherTemp);
     //HUMIDITY
     var weatherHumid = data.main.humidity;
-    console.log(weatherHumid);
     //WIND
     var weatherWind = data.wind.speed;
-    console.log(weatherWind);
+    //UV INDEX LONGITUDE
+    var uvIndexlon = data.coord.lon;
+    //UV INDEX LATITUDE
+    var uvIndexLat = data.coord.lat;
+    
 
     //display the info
+    // descriptionId.textContent = weatherDes + "|" + weatherIcon;
     tempId.textContent = weatherTemp + " °F";
     humidId.textContent = weatherHumid + " %";
     windId.textContent = weatherWind + " mph";
     
-
-
-    //We need the UV Index
+    // //We need the UV Index
+    var uvApi = "https://api.openweathermap.org/data/2.5/uvi?appid=7d0096a167ea1572df8a347cb4cb0104&lat="+ uvIndexLat + "&lon=" + uvIndexlon;
+    fetch(uvApi)
+        .then(function(response){
+            if(response.ok){
+                response.json().then(function(data){
+                    var uvValue = data.value;
+                    uvId.textContent = uvValue;
+                })
+            }
+        })
 
 
 }
+
 
 searchBtn.addEventListener("click", searchFormHandler);
